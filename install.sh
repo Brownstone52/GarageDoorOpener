0e1fbbcbde5b6c5297a3322aed7919483c0d18ae
Mike Brown <michael.c.brown87@gmail.com>
2016-09-04 13:17:30 +0000

    updating install files

--- install.sh
+++ install.sh
@@ -7,8 +7,17 @@ apt-get install python
 echo "Installing python package manager"
 apt-get install python-pip
 
+echo "Installing python-dev"
+apt-get install build-essential python-dev
+
 echo "Resolving python dependencies"
 pip install -r reqs.txt
 
 echo "Installing GPIO library"
 apt-get install rpi.gpio
+
+echo "Cloning DHT library"
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+
+echo "Installing DHT library"
+python Adafruit_Python_DHT/setup.py install
