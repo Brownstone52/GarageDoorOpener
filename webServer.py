ac5988ebd493b664c01b6a006d0529a299d298ec
Mike Brown <michael.c.brown87@gmail.com>
2017-04-28 09:48:49 +0000

    Added temperature logic

--- webServer.py
+++ webServer.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 from garage import Garage
+from Temperature import GarageEnvironment
 
 app = Flask(__name__)
 
 garage = Garage()
+garageEnvironment = GarageEnvironment()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -14,6 +17,11 @@ def index():
 def openGarage():
 	garage.PressGarageDoorButton()
 	return ""
+
+@app.route('/temperatureData', methods=['GET'])
+def getTemperatureData():
+	garageEnvironment.FetchData()
+	return jsonify(temperature=garageEnvironment.Temperature, humidity=garageEnvironment.Humidity)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
