eba93083bd1254f0090ca0ba2d572dfb074639e7
Mike Brown <michael.c.brown87@gmail.com>
2017-05-10 10:59:16 +0000

    Don't use debug version of flask

--- webServer.py
+++ webServer.py
@@ -24,4 +24,4 @@ def getTemperatureData():
 	return jsonify(temperature=garageEnvironment.Temperature, humidity=garageEnvironment.Humidity)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=80)
