5079cb57ac4cccbea6663b119caec4b77a841a76
Mike Brown <michael.c.brown87@gmail.com>
2017-05-15 00:52:02 +0000

    Added humidity view

--- static/view.js
+++ static/view.js
@@ -2,7 +2,7 @@ class TemperatureView {
 	render() {
 		$.getJSON("temperatureData", (data)=> {
 			let temp = new Temperature(data.temperature).Farenheit.toFixed(2)
- 			$("#open").text(temp)
+ 			$("#open").text(`${temp} F`)
 
 			let rgb = new TemperatureToColorMapper().getColor(temp)
 			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 0.7)`
@@ -12,6 +12,27 @@ class TemperatureView {
 		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=43.792687&lon=-70.430532&appid=1963a6b41a6c18337d5160732408f58a", (data)=>{
 			let temp = Temperature.fromKelvin(data.main.temp).Farenheit.toFixed(2)
 			let rgb = new TemperatureToColorMapper().getColor(temp)
+			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 0.7)`
+
+			$("body").css({"background-color": rgbCss})
+		})
+	}
+}
+
+class HumidityView {
+	render() {
+		$.getJSON("temperatureData", (data)=> {
+			let humidity = data.humidity.toFixed(2)
+			$("#open").text(`${humidity}%`)
+
+			let rgb = new HumidityToColorMapper().getColor(humidity)
+			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 0.7)`
+			$("#open").css({"background-color": rgbCss}).css({"height": "100%"}).css({"font-size": "5em"})
+		})
+
+		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=43.792687&lon=-70.430532&appid=1963a6b41a6c18337d5160732408f58a", (data)=>{
+			let humidity = data.main.humidity.toFixed(2)
+			let rgb = new HumidityToColorMapper().getColor(humidity)
 			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 0.7)`
 
 			$("body").css({"background-color": rgbCss})
