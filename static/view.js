fe649b392159d77541d85c9967ed80259a1dbb69
Mike Brown <michael.c.brown87@gmail.com>
2017-05-16 11:25:15 +0000

    Make button totally opaque

--- static/view.js
+++ static/view.js
@@ -5,14 +5,14 @@ class TemperatureView {
  			$("#open").text(`${temp} F`)
 
 			let rgb = new TemperatureToColorMapper().getColor(temp)
-			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 0.7)`
+			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 1)`
 			$("#open").css({"background-color": rgbCss}).css({"height": "100%"}).css({"font-size": "5em"})
 		})
 
 		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=43.792687&lon=-70.430532&appid=1963a6b41a6c18337d5160732408f58a", (data)=>{
 			let temp = Temperature.fromKelvin(data.main.temp).Farenheit.toFixed(2)
 			let rgb = new TemperatureToColorMapper().getColor(temp)
-			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 0.7)`
+			let rgbCss = `rgba(${Math.round(rgb.r)}, ${Math.round(rgb.g)}, ${Math.round(rgb.b)}, 1)`
 
 			$("body").css({"background-color": rgbCss})
 		})
