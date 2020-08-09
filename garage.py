88d575996203b61263fa7e19b960c9e31960d0a2
Michael C Brown <michael.c.brown87@gmail.com>
2020-08-09 11:46:41 -0400

    change output

--- garage.py
+++ garage.py
@@ -5,11 +5,11 @@ class Garage:
 
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setup(7, GPIO.OUT)
-	GPIO.output(7, GPIO.HIGH)
+	GPIO.output(7, GPIO.LOW)
 	def sayHello(self):
 		return "HELLO WORLD"
 	
 	def PressGarageDoorButton(self):
-		GPIO.output(7, GPIO.LOW)
-		time.sleep(2)
 		GPIO.output(7, GPIO.HIGH)
+		time.sleep(2)
+		GPIO.output(7, GPIO.LOW)
